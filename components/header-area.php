c3feaa1d2c737aadca305ef75dc984167f8c384c
Handy Dannu Wijaya <handydannu@users.noreply.github.com>
2020-10-07 09:33:06 +0700

    update repo

---
+++ components/header-area.php
@@ -0,0 +1,34 @@
+<div class="container">
+
+  <!-- ads top -->
+  <div id="fix-banner-top">
+    <a href="#" target="_blank" title="AyoSurabaya">
+      <img class="img-fluid" src="https://cdn.ayobandung.com/upload/bank_image/medium/BJB_Amazing_Sureprize.png?w=1197" class="img-responsive" title="" style="">
+    </a>
+  </div><!-- end ads top -->
+
+    <div class="row"><!-- header utama --> 
+
+      <div class="col-lg-3 pt-3 mt-3 text-center"><!-- medsos area -->
+        <div class="medsos mt-2">
+          <a href="#" class="medsos-icon fb-bg"><i class="fab fa-facebook-f fa-2x"></i></a>
+          <a href="#" class="medsos-icon twt-bg"><i class="fab fa-twitter fa-2x"></i></a>
+          <a href="#" class="medsos-icon ig-bg"><i class="fab fa-instagram fa-2x"></i></a>
+          <a href="#" class="medsos-icon yt-bg"><i class="fab fa-youtube fa-2x"></i></a>
+          <a href="#" class="medsos-icon rss-bg"><i class="fas fa-rss fa-2x"></i></a>
+        </div>  
+      </div><!-- end medsos area -->
+
+      <div class="col-lg-6 pt-3 mt-3 text-center"><!-- logo area -->
+        <a href="#"><img class="img-fluid" src="https://www.ayosurabaya.com/assets/img/ayosurabaya-logo.png?w=400"></a>
+        <div class="logo-text">SEMUA TENTANG SURABAYA</div>
+      </div><!-- end logo area -->
+      
+      <div class="col-lg-3 pt-3 mt-3 text-center"><!-- date area -->
+          <div class="bg-date-nav">
+            <i class="fas fa-calendar"></i> <span id="tanggalwaktu"></span>
+          </div>
+      </div><!-- end area -->
+
+    </div> <!-- end row header utama -->
+</div>
---
+++ components/header-scripts.php
@@ -0,0 +1,12 @@
+<title>AyoSurabaya.com - Semua Tentang Surabaya</title>
+
+<!-- meta titles -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- css -->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/css/surabaya.css">
+<link rel="stylesheet" href="assets/fontawesome/css/all.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed&display=swap" rel="stylesheet">
+<link rel="icon" href="https://www.ayosurabaya.com/assets/img/footer-logo.png" type="image/x-icon"/ >
