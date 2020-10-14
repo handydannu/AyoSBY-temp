971af716954fca2476a579d7419782f27941f463
Handy Dannu Wijaya <handydannu@users.noreply.github.com>
2020-10-14 15:29:05 +0700

    changes 14 okt

--- backup/application/views/metamorph/home-ads-2.php
+++
@@ -1,8 +0,0 @@
-
-         <div id="fix-banner-top">
-              <a href="http://www.bankbjb.co.id/" target="_blank" title="">
-                <img src="<?php site_url();?>assets/img/widget/bjb.jpg?w=1197" class="img-responsive" />
-              </a>
-       </div>
-            <div class="clearfix"></div>
-
--- backup/application/views/metamorph/home-ads-footer.php
+++
@@ -1,7 +0,0 @@
-<section class="col-banner-display col-md-12 col-xs-12 mg-top-10">
-    <div id="banner-display-2">
-		<a href="http://ayobandung.com/?q=dprd&sa=" target="_blank" title="Kabar DPRD Jabar">
-			<img src="/assets/ads/2018/08/73-pemprov-jabar-15-08-18930x91.jpg" class="img-responsive" />
-		</a>
-	</div>
-</section>
